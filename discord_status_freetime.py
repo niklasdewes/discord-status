255dcea7f7611136f4d458c1584533b2ec936390
Niklas Dewes <niklas.dewes@icloud.com>
2022-02-18 18:58:33 +0100

    add requirements.txt, freetime and working script

---
+++ discord_status_freetime.py
@@ -0,0 +1,17 @@
+import requests
+
+def main():
+    
+    user = DiscordStatusFreetime
+
+class DiscordStatusFreetime:
+    ''' Login information format
+    {
+        "username": "test@email.com",
+        "password": "T3stP4ssw0rd.",
+        "url": "https://discord.com/login"
+    }
+    '''
+
+    def change_status_freetime():
+        pass
---
+++ discord_status_working.py
@@ -0,0 +1,16 @@
+import requests
+
+def main():
+    user = DiscordStatusWorking
+
+class DiscordStatusWorking:
+    ''' Login information format
+    {
+        "username": "test@email.com",
+        "password": "T3stP4ssw0rd.",
+        "url": "https://discord.com/login"
+    }
+    '''
+
+    def change_status_working(self):
+        pass
